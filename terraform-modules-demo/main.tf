c7012807da9f9257514cfcbadb89c6f574541bc6
Sakshi Gautam <gautamsakshi551@gmail.com>
2025-10-28 16:33:28 +0530

    Update main.tf

--- terraform-modules-demo/main.tf
+++ terraform-modules-demo/main.tf
@@ -1,1 +1,11 @@
+module "web_server" {
+  source         = "./modules/ec2_instance"
+  ami_id         = "ami-0c55b159cbfafe1f0"
+  instance_type  = "t2.micro"
+  instance_name  = "MyWebServer"
+}
+
+output "web_server_id" {
+  value = module.web_server.instance_id
+}
 
